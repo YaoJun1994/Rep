6641fa2b774d6e5d013c943c4b53421842660c05
YaoJun1994 <yaojun_world@outlook.com>
2021-10-24 20:16:02 +0800

    1

--- src/project/task.cpp
+++ src/project/task.cpp
@@ -1,1 +1,18 @@
 #include "task.hpp"
+
+unsigned int Task::RUNNINGTASKNUM = 0;
+
+void Task::setFuncName(std::function<int(std::string)> newName)
+{
+    func = newName;
+}
+
+void Task::setParam(const std::string& file)
+{
+    paramFile = file;
+}
+
+void Task::execFunc()
+{
+    returnCode = func(this->paramFile);
+}
--- src/project/task.hpp
+++ src/project/task.hpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include <string>
+#include <functional>
 
 #ifndef __TASK
 #define __TASK
 
+class Task
+{
+public:
+    //任务函数名绑定
+    void setFuncName(std::function<int(std::string)>);
+    //设置任务参数
+    void setParam(const std::string&);
+    //执行任务
+    void execFunc();
+
+private:
+    //任务函数名
+    std::function<int(std::string)> func;
+    //返回码
+    int returnCode;
+    //错误信息
+    std::string retErrInfo;
+    //参数
+    std::string paramFile;
+    //运行中任务数
+    static unsigned int RUNNINGTASKNUM;
+};
 
 #endif
